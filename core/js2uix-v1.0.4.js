eae0ed78c521ea8e9e224b3843e260e8f974fc76
YJH-js2uix <deshineplus@naver.com>
2018-06-08 10:09:17 +0900

    js2uix - develop

--- core/js2uix-v1.0.4.js
+++ core/js2uix-v1.0.4.js
@@ -29,7 +29,7 @@
     if( !window || typeof window === 'undefined'){ return false;}
     var js2uix;
     var ModuleName = 'js2uix';
-    var ModuleVersion = 'v1.0.0';
+    var ModuleVersion = 'v1.0.4';
     var DOC = window.document;
     var SetProtoType = Object.setPrototypeOf;
     var GetProtoType = Object.getPrototypeOf;
